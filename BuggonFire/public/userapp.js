17a223a2587aae3ee7b2b0cc54c400e71c599cf3
wb28pa <wb28pa@gmail.com>
2019-07-09 12:42:10 -0400

    V2 with frontend

---
+++ BuggonFire/public/userapp.js
@@ -0,0 +1,17 @@
+
+function googleLogin() {
+                            
+    const app = firebase.app();                     
+                          
+    const provider = new firebase.auth.GoogleAuthProvider();
+    
+    firebase.auth()signInWithPopup(provider)
+   
+        .then(result {
+          const user = result.user;
+          document.write('Hello, ${user.displayName}')
+            console.log(user)
+
+          })
+          .catch(console.log)
+}
